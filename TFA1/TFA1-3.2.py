50e3a6f4f5951621ff3caa616aa117f84035df47
Norwood Shane Banzuela <norwood0602@gmail.com>
2025-02-03 09:45:03 +0800

    Second Activity Done

---
+++ TFA1/TFA1-3.2.py
@@ -0,0 +1,7 @@
+firstName = input('Enter your first name: ')
+lastName = input('Enter your last name: ')
+fullName = str(firstName + lastName)
+print('Full Name: ' + firstName + ' ' + lastName)
+print('Full Name (Upper Case): ' + firstName.upper() + ' ' + lastName.upper())
+print('Full Name (Lower Case): ' + firstName.lower() + ' ' + lastName.lower())
+print('Length of Full Name: ' + str(len(fullName)))
