2b4082a459d47f87899af546bb049e9f5756148f
Norwood Shane Banzuela <norwood0602@gmail.com>
2025-02-03 10:14:33 +0800

    Done

--- TFA1/TFA1-3.4.py
+++ TFA1/TFA1-3.4.py
@@ -1,13 +1,6 @@
-firstName = input('Enter first name: ')
-lastName = input('Enter last name: ')
-age = input('Enter age: ')
-conNum = input('Enter contact number: ')
-course = input('Enter course: ')
+print('Reading Student Information:')
+f = open('TFA1/students.txt', 'r')
 
-formattedText = ('Last Name: {} \nFirst Name: {} \nAge: {} \nContact Number: {} \nCourse: {} \n')
-f = open('TFA1/students.txt', 'a+')
-f.write(formattedText.format(lastName, firstName, age, conNum, course))
-line = f.readline()
+for content in f:
+    print(content)
 f.close()
-
-print('Information has been saved to \'students.txt\'.')
