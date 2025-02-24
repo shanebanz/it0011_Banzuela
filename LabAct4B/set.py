42ddf8b50f4f4069804c5638c1fb604d8c5c3cf1
Norwood Shane Banzuela <167977392+shanebanz@users.noreply.github.com>
2025-02-24 14:49:19 +0800

    Nice Progress

--- LabAct4B/set.py
+++ LabAct4B/set.py
@@ -5,7 +5,10 @@ C = {'c', 'd', 'f', 'h', 'i', 'j', 'k'}
 
 # a. How many elements are there in set A and B
 elementsTotal = len(A) + len(B);
-print(elementsTotal)
+print("Total number of elements in set A and B: " + str(elementsTotal))
 # b. How many elements are there in B that is not part of A and C
+elementsB = B - (A | C)  
+elementCntr = len(elementsB)
+print("Total number of elements in set B that is not part of A and C is: " + str(elementCntr))  
 
 # c. Show the following using set operations
