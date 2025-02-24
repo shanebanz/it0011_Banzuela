6708579a2fc864eb3d7d0d7085b02497491b623a
Norwood Shane Banzuela <167977392+shanebanz@users.noreply.github.com>
2025-02-24 14:37:49 +0800

    Inital Changes Before Submission

---
+++ LabAct4B/set.py
@@ -0,0 +1,11 @@
+# Our Sets
+A = {'a', 'b' , 'c', 'd' , 'f' , 'g'}
+B = {'c', 'b', 'h', 'l', 'm', 'o'}
+C = {'c', 'd', 'f', 'h', 'i', 'j', 'k'}
+
+# a. How many elements are there in set A and B
+elementsTotal = len(A) + len(B);
+print(elementsTotal)
+# b. How many elements are there in B that is not part of A and C
+
+# c. Show the following using set operations
---
+++ numberPattern.py
@@ -0,0 +1,6 @@
+rows = 5
+
+for i in range(0, rows):
+    for j in range(i+1):
+        print(j+1, end=' ')
+    print() 
