8ba9d4e6d796f0a4cc6a6016f5e9f307a3e32245
Norwood Shane Banzuela <167977392+shanebanz@users.noreply.github.com>
2025-03-01 21:16:39 +0800

    SET Done

--- LabAct4B/set.py
+++ LabAct4B/set.py
@@ -12,3 +12,16 @@ elementCntr = len(elementsB)
 print("Total number of elements in set B that is not part of A and C is: " + str(elementCntr))  
 
 # c. Show the following using set operations
+# & FOR INTERSECTION, - TO MINUS OR REMOVE A CERTAIN SET, | IS THE UNION, sorted() to sort it from a - z
+# i. [h, i, j, k]
+print(sorted(C - A))
+# ii. [c, d, f]
+print(sorted(A & C))
+# iii. [b, c, h]
+print(sorted((A & B) | (B & C)))
+# iv. [d, f]
+print(sorted(A & C - B))
+# v. [c]
+print(sorted(A & B & C))
+# vi. [l, m, o]
+print(sorted(B - (A | C)))
